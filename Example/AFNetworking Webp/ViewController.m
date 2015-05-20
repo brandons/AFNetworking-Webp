cbc84849e3e99bd9a669fdf6738c32c17c31d0a2
brandons <brandon@schlenker.im>
2015-05-20 09:15:59 -0700

    Example

---
+++ Example/AFNetworking Webp/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  AFNetworking Webp
+//
+//  Created by Brandon on 5/20/15.
+//  Copyright (c) 2015 Brandon Schlenker. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFWebPImageResponseSerializer.h"
+#import <AFNetworking/UIImageView+AFNetworking.h>
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    AFWebPImageResponseSerializer *imageSerializer = [[AFWebPImageResponseSerializer alloc] init];
+    self.imageView.imageResponseSerializer = imageSerializer;
+    
+    [self.imageView setImageWithURL:[NSURL URLWithString:@"http://res.cloudinary.com/yerdle/image/upload/v1432133403/lhfer5lguvi76a9uedez.webp"] placeholderImage:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
