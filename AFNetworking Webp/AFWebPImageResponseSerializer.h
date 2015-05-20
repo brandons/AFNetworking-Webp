127d0a2c15db561fc9c6dc397cffd5737be2d787
brandons <brandon@schlenker.im>
2015-05-20 09:14:56 -0700

    AFWebPImageResponseSerializer.h

---
+++ AFNetworking Webp/AFWebPImageResponseSerializer.h
@@ -0,0 +1,13 @@
+//
+//  AFWebPImageResponseSerializer.h
+//  AFNetworking Webp
+//
+//  Created by Brandon on 5/20/15.
+//  Copyright (c) 2015 Brandon Schlenker. All rights reserved.
+//
+
+#import "AFURLResponseSerialization.h"
+
+@interface AFWebPImageResponseSerializer : AFImageResponseSerializer
+
+@end
